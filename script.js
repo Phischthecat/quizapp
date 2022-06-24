30241d26dbdfc1a4ff2e5a4f5dbeb5a5889d32fe
Phil Schmucker <p.schmuck.er@web.de>
2022-06-24 20:15:28 +0200

    all functions implemented

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-let questions = [
-  {
-    question: 'Wer hat HMTL erfunden?',
-    answer_1: 'Robbie Williams',
-    answer_2: 'Lady Gaga',
-    answer_3: 'Tim Berners-Lee',
-    answer_4: 'Justin Bieber',
-    right_answer: 3,
-  },
-  {
-    question: 'Was bedeutet das HTML Tag &lt;a&gt;?',
-    answer_1: 'Text Fett',
-    answer_2: 'Container',
-    answer_3: 'Ein Link',
-    answer_4: 'Kursiv',
-    right_answer: 3,
-  },
-  {
-    question: 'Wie stellt man Text am BESTEN fett dar?',
-    answer_1: '&lt;strong&gt;',
-    answer_2: 'CSS nutzen',
-    answer_3: '&lt;bold&gt;',
-    answer_4: '&lt;b&gt;',
-    right_answer: 1,
-  },
-  {
-    question: 'Welches Attribut kann man NICHT für Textarea verwenden?',
-    answer_1: 'readonly',
-    answer_2: 'max',
-    answer_3: 'from',
-    answer_4: 'spellcheck',
-    right_answer: 1,
-  },
-  {
-    question: 'Wie bindet man eine Website in eine Website ein?',
-    answer_1: '&lt;iframe&gt;,&lt;frame&gt;, and &lt;frameset&gt;,',
-    answer_2: '&lt;iframe&gt;',
-    answer_3: '&lt;frame&gt;',
-    answer_4: '&lt;frameset&gt;',
-    right_answer: 2,
-  },
-  {
-    question:
-      'Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem Attribut title aus?',
-    answer_1: 'a[title] {...}',
-    answer_2: 'a > [title] {...}',
-    answer_3: 'a.title {...}',
-    answer_4: 'a = title {...}',
-    right_answer: 1,
-  },
-  {
-    question: 'Wie definiert man in Javascript eine Variable?',
-    answer_1: 'let 100 = rate;',
-    answer_2: '100 = let rate',
-    answer_3: 'rate = 100',
-    answer_4: 'let rate = 100',
-    right_answer: 4,
-  },
-];
-
 let currentQuestion = 0;
+let rightQuestions = 0;
+let selectedQuiz = questions_html;
 
 function init() {
   document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
-  document.getElementById('all-questions').innerHTML = questions.length;
+  document.getElementById('all-questions').innerHTML = selectedQuiz.length;
+
   showQuestion();
+  popOver();
+}
+
+function changeQuiz(select) {
+  selectedQuiz = select;
+  replay();
+}
+
+function activeHTML(select) {
+  addActive('html');
+  removeActive('handball');
+  removeActive('allgemein');
+  console.log(select);
+  changeQuiz(select);
+}
+
+function activeHandball(select) {
+  removeActive('html');
+  addActive('handball');
+  removeActive('allgemein');
+  console.log(select);
+  changeQuiz(select);
+}
+
+function activeAllgemein(select) {
+  removeActive('html');
+  removeActive('handball');
+  addActive('allgemein');
+  console.log(select);
+  changeQuiz(select);
+}
+
+function removeActive(id) {
+  document.getElementById(id).classList.remove('active');
+}
+
+function addActive(id) {
+  document.getElementById(id).classList.add('active');
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
+  let question = selectedQuiz[currentQuestion];
   document.getElementById('question').innerHTML = question['question'];
   document.getElementById('answer_1').innerHTML = question['answer_1'];
   document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -77,38 +58,89 @@ function showQuestion() {
 }
 
 function answer(selection) {
-  let right_answer = questions[currentQuestion]['right_answer'];
+  let right_answer = selectedQuiz[currentQuestion]['right_answer'];
   let idOfRightAnswer = 'answer_' + right_answer;
   if (selection.match(right_answer)) {
-    document.getElementById(selection).parentNode.classList.add('bg-success');
+    document
+      .getElementById(selection)
+      .parentNode.classList.add('right-answer-card');
+    document
+      .getElementById('strong-' + selection)
+      .classList.add('right-answer');
+    rightQuestions++;
   } else {
-    document.getElementById(selection).parentNode.classList.add('bg-danger');
+    document
+      .getElementById(selection)
+      .parentNode.classList.add('wrong-answer-card');
+    document
+      .getElementById('strong-' + selection)
+      .classList.add('wrong-answer');
     document
       .getElementById(idOfRightAnswer)
-      .parentNode.classList.add('bg-success');
+      .parentNode.classList.add('right-answer-card');
+    document
+      .getElementById('strong-' + idOfRightAnswer)
+      .classList.add('right-answer');
   }
   document.getElementById('nextBtn').disabled = false;
 }
 
 function nextQuestion() {
   currentQuestion++;
-  if (currentQuestion >= questions.length) {
+  if (currentQuestion >= selectedQuiz.length) {
     document.getElementById('endscreen').style = '';
     document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('all-questions-endscreen').innerHTML =
+      selectedQuiz.length;
+    document.getElementById('all-right-questions').innerHTML = rightQuestions;
   } else {
     document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
     showQuestion();
     resetAnswerButtons();
   }
+  percent();
 }
 
 function resetAnswerButtons() {
   for (let i = 1; i <= 4; i++) {
     document
       .getElementById('answer_' + i)
-      .parentNode.classList.remove('bg-success');
+      .parentNode.classList.remove('right-answer-card');
     document
       .getElementById('answer_' + i)
-      .parentNode.classList.remove('bg-danger');
+      .parentNode.classList.remove('wrong-answer-card');
+    document
+      .getElementById('strong-answer_' + i)
+      .classList.remove('right-answer');
+    document
+      .getElementById('strong-answer_' + i)
+      .classList.remove('wrong-answer');
   }
+}
+
+function percent() {
+  let percent = (currentQuestion / selectedQuiz.length) * 100;
+
+  document.getElementById('progress').style = `width: ${percent}%`;
+}
+
+function replay() {
+  currentQuestion = 0;
+  rightQuestions = 0;
+  document.getElementById('endscreen').style = 'display: none';
+  document.getElementById('questionBody').style = '';
+  document.getElementById('all-questions-endscreen').innerHTML =
+    selectedQuiz.length;
+  resetAnswerButtons();
+  init();
+  percent();
+}
+
+function popOver() {
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  const popoverList = [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+  );
 }
